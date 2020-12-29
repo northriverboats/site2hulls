db5aa3267bb7192fd685f1ab5cd831e37638e335
Fredrick W. Warren <fredw@northriverboats.com>
2020-12-29 09:40:36 -0800

    added Provinces to State table

--- site2hulls.py
+++ site2hulls.py
@@ -72,7 +72,20 @@ states = {
     'Washington': 'WA',
     'Wisconsin': 'WI',
     'West Virginia': 'WV',
-    'Wyoming': 'WY'
+    'Wyoming': 'WY',
+    'Newfoundland and Labrador': 'NL',
+    'Prince Edward Island': 'PE',
+    'Nova Scotia': 'NS',
+    'New Brunswick': 'NB',
+    'Quebec': 'QC',
+    'Ontario': 'ON',
+    'Manitoba': 'MB',
+    'Saskatchewan': 'SK',
+    'Alberta': 'AB',
+    'British Columbia': 'BC',
+    'Yukon': 'YT',
+    'Northwest Territories': 'NT',
+    'Nunavut': 'NU',
 }
 
 """
